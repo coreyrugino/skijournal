91da1a69218ede32edd270d8ef964d4624aa5940
Corey Rugino <coreyrugino@gmail.com>
2016-05-14 21:28:34 -0600

    working instagram threw ajax to react receiving in data

--- app/assets/javascripts/components/Instapic.js.jsx
+++ app/assets/javascripts/components/Instapic.js.jsx
@@ -7,15 +7,13 @@ class Instapic extends React.Component{
   render(){
     return(
       <div>
-        <div className='col s12 m4'>
-          <div className='z-depth-5 card hoverable green' onClick={()=>this.showInfo()} id='journalCards'>
-            <div className='card-content white-text'>
-              <span className='card-title'>{this.props.title}</span>
-              <h3>{this.props.date}</h3>
-              <p className='flow-text truncate'>{this.props.story}</p>
-              <p>{this.props.partners}</p>
-              <a href='#' className='black' onClick={()=>this.deleteItem()} >X</a>
-            </div>
+        <div className='z-depth-5 card hoverable blue' onClick={()=>this.showInfo()} id='journalCards'>
+          <div className='card-content white-text'>
+            <span className='card-title'>{this.props.title}</span>
+            <h3>{this.props.date}</h3>
+            <p className='flow-text truncate'>{this.props.story}</p>
+            <p>{this.props.caption.text}</p>
+            <a href='#' className='black' onClick={()=>this.deleteItem()} >X</a>
           </div>
         </div>
       </div>);
