0d70f3be91ef38982fc2ebfb95d94221d7ce1ab5
Corey Rugino <coreyrugino@gmail.com>
2016-08-30 16:15:06 -0600

    2nd heroku push

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,6 +1,6 @@
-class InstagramController < ApplicationController
-  def index
-    @instagram = Instagram.user_recent_media({:count => 2})
-    # @instagram = Instagram.media_search("37.7808851", "-122.3948632")
-  end
-end
+# class InstagramController < ApplicationController
+#   def index
+#     @instagram = Instagram.user_recent_media({:count => 2})
+#     # @instagram = Instagram.media_search("37.7808851", "-122.3948632")
+#   end
+# end
