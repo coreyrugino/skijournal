0f3a090ae8d019b18757855eafdee133a785da09
Corey Rugino <coreyrugino@gmail.com>
2016-09-23 21:17:28 -0600

    heroku problems

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,14 +2,14 @@
 
 require 'carrierwave/orm/activerecord'
 
-if Rails.env.development?
-  CarrierWave.configure do |config|
-    config.storage = :file
-  end
-end
-
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.storage = :fog
-  end
-end
+# if Rails.env.development?
+#   CarrierWave.configure do |config|
+#     config.storage = :file
+#   end
+# end
+#
+# if Rails.env.production?
+#   CarrierWave.configure do |config|
+#     config.storage = :fog
+#   end
+# end
